96aa254ef38a843ead0cd28ea2d456388fefcbad
Fausto Rosado <faustrosado@gmail.com>
2019-01-22 08:19:43 -0500

    defined change method in create-users migrate file

--- db/migrate/20190122131140_create_users.rb
+++ db/migrate/20190122131140_create_users.rb
@@ -1,4 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
+    create_table :users do |t|
+      t.string :email
+      t.string :password
+      t.string :firstname
+      t.string :lastname
+      t.datetime :birthday
   end
 end

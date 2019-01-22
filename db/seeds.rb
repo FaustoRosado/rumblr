3afe9714194a67e99937402827cb88f8537c858d
Fausto Rosado <faustrosado@gmail.com>
2019-01-22 09:41:35 -0500

    added users in seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+User.create(
+  email: 'fausto@gmail.com',
+  password: 'password',
+  firstname: 'Fausto',
+  lastname: 'R',
+  birthday: '09/07/1980'
+)
+
+User.create(
+  email: 'bola@gmail.com',
+  password: 'password',
+  firstname: 'Bola',
+  lastname: 'B',
+  birthday: '01/01/1970'
+)
+
+User.create(
+  email: 'samantha@gmail.com',
+  password: 'password',
+  firstname: 'Samantha',
+  lastname: 'SR',
+  birthday: '09/25/1980'
+)

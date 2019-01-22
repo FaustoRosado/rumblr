ca0097a37b3b91516a542cd9498038971dd515b6
Fausto Rosado <faustrosado@gmail.com>
2019-01-22 08:23:32 -0500

    defined change methodin create_posts migrate file

--- db/migrate/20190122132057_create_posts.rb
+++ db/migrate/20190122132057_create_posts.rb
@@ -1,4 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :user_id
+      t.string :image_url
+      t.datetime :datetime
   end
 end

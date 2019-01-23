d709a59555a21f026365bf20f0ebf4b8d8fee9e3
Fausto Rosado <faustrosado@gmail.com>
2019-01-22 21:30:13 -0500

    changes in app.erb

--- app.rb
+++ app.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra/flash'
+# require 'sinatra/reloader'
+# to add pagination see paginate gem
+
+# require_relative 'validate'
+# require 'will_paginate'
+# require 'will_paginate/active_record'
+
 require_relative 'models'
 
-get "/" do
-Testing
+enable :sessions
+
+get '/' do
+  begin
+    @posts = Post.all
+    
+  rescue
+    @posts = nil
+  end
+
+  erb :index
 end
+
+

364d3bba61200ae401d7e2b123c413c7c393065a
Fausto Rosado <faustrosado@gmail.com>
2019-01-22 21:56:05 -0500

    create validate.rb and create class Validate with three method functions

---
+++ validate.rb
@@ -0,0 +1,13 @@
+class Validate
+  def self.login(email, password)
+    
+  end
+
+  def self.register(email, password, firstname, lastname, birthday)
+    
+    
+  end
+
+  def self.post(title)
+    
+end

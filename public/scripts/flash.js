1f6b7c8ece52c6b3b7413dd676600dc2f386978c
Fausto Rosado <faustrosado@gmail.com>
2019-01-23 06:26:23 -0500

    added js onclick event to close flash messages

--- public/scripts/flash.js
+++ public/scripts/flash.js
@@ -1,1 +1,12 @@
-if (document.querySelector('.remove-btn'))
+if (document.querySelector('.remove-btn')) {
+  const removeBtn = document.querySelector('.remove-btn');
+
+  const close = () => {
+      const toDelete = document.querySelector('.flash'),
+          wrapper = document.getElementById('wrapper');
+
+      wrapper.removeChild(toDelete);
+  };
+
+  removeBtn.onclick = close;
+}
